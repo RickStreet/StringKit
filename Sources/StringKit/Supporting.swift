7c0394bc2ac2b32e7ab00760632daad37c29d14d
RickStreet <Rick_1@me.com>
2021-10-20 10:35:20 -0700

    Add NSAttributedString attributes

---
+++ Sources/StringKit/Supporting.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rick Street on 10/20/21.
+//
+
+import Cocoa
+
+public let lightYellow = NSColor(red: 255.0/255.0, green: 255.0/255.0, blue: 198.0/255.0, alpha: 1.0)
+public let darkGrey = NSColor(red: 127.0/255.0, green: 127.0/255.0, blue: 127.0/255.0, alpha: 1.0)
+public let lightGrey = NSColor(red: 237.0/255.0, green: 237.0/255.0, blue: 237.0/255.0, alpha: 1.0)
+
+public let pink = NSColor(red: 255.0/255.0, green: 231.0/255.0, blue: 235.0/255.0, alpha: 1.0)
+public let darkRed = NSColor(red: 137.0/255.0, green: 33.0/255.0, blue: 16.0/255.0, alpha: 1.0)
+public let navy = NSColor(red: 4.0/255.0, green: 30.0/255.0, blue: 141.0/255.0, alpha: 1.0)
+public let forestGreen = NSColor(red: 0.0/255.0, green: 153.0/255.0, blue: 76.0/255.0, alpha: 1.0)
+public let black = NSColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+
+
+var headerParagraphStyle = NSMutableParagraphStyle()
+
+var normalParagraphStyle = NSMutableParagraphStyle()
+
+var convParagraphStyle = NSMutableParagraphStyle()
+
+var titleAttribute = [ NSAttributedString.Key.foregroundColor: navy,
+                      NSAttributedString.Key.font: NSFont(name: "HelveticaNeue-BoldItalic", size: 20.0)!]
+
+var headerAttribute = [ NSAttributedString.Key.foregroundColor: navy,
+                       NSAttributedString.Key.font: NSFont(name: "HelveticaNeue-Italic", size: 12.0)!]
+
+var normalAttribute = [ NSAttributedString.Key.foregroundColor: black,
+                        NSAttributedString.Key.font: NSFont(name: "HelveticaNeue", size: 10.0)!]
+/*
+var convAttributes = [ NSAttributedString.Key.foregroundColor: black,
+                       NSAttributedString.Key.font: NSFont(name: "HelveticaNeue", size: 10.0)!]
+*/
+
+let smallAttribute = [ NSAttributedString.Key.foregroundColor: navy, NSAttributedString.Key.font: NSFont(name: "HelveticaNeue", size: 6.0)!]
+
