8c0cb0ff58dba8ba1d363f1697dc62d5e9e72156
Rick Street <rick_1@md.com>
2025-11-06 11:09:44 -0800

    Added fullTagDescription and shortTagDescription

--- Sources/StringKit/StringExtensions.swift
+++ Sources/StringKit/StringExtensions.swift
@@ -437,7 +437,45 @@ public extension String {
         }
         return self.capitalized
     }
+    
+    /// Create full description for tags
+    /// - Parameters:
+    ///   - name: Tag Name
+    ///   - description: Tag Desdription
+    ///   - units: tag engineering limits
+    /// - Returns: description with name, description, and units
+    func fullTagDescription(name: String, description: String, units: String) -> String {
+        var string = ""
+        if name.isEmpty {
+            string = description
+        } else {
+            if description.isEmpty {
+                string = name
+            } else {
+                string = "\(name), \(description)"
+            }
+        }
+        if !units.isEmpty {
+            string += ", \(units)"
+        }
+        return string
+    }
+    
+    /// Short tag description
+    /// - Parameters:
+    ///   - name: Tag Name
+    ///   - Description: Tag Description
+    /// - Returns: tag name or dexcription of there is no name
+    func shortTagDescription(name: String, Description: String) -> String {
+        if !name.isEmpty {
+            return name
+        } else {
+            return Description
+        }
+    }
 
+
+    
     /**
      Pads string with spaces to right
      - parameters:
